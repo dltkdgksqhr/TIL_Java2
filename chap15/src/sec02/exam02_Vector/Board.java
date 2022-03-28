55e95f724c854c0343cf28bcb1a3191c64e38051
dltkdgksqhr <78789ks@naver.com>
2022-03-28 15:55:33 +0900

    22/03/08  15.2.2절 Vector

---
+++ chap15/src/sec02/exam02_Vector/Board.java
@@ -0,0 +1,16 @@
+package sec02.exam02_Vector;
+
+public class Board {
+  String subject;
+  String content;
+  String writer;
+  
+  public Board(String subject, String content, String writer) {
+	this.subject = subject;
+	this.content = content;
+	this.writer = writer;
+}
+  
+  
+  
+}
