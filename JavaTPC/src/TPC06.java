1386fd4eff97a01bfaff6bdfb461ea9f8879954f
dltkdgksqhr <78789ks@naver.com>
2022-03-29 11:20:39 +0900

    22/03/29  TPC06 메서드는 변수다

--- JavaTPC/src/TPC06.java
+++ JavaTPC/src/TPC06.java
@@ -5,23 +5,26 @@ public class TPC06 {
    // 5. 메서드 ->  동작(method), 기능(function)
     int total = sum(10,20);
     System.out.println(total);
-    int[] arr = makeArr();
-    int hap=0;
-    for(int i=0;i<arr.length;i++) {
-    	hap += arr[i];
+    
+    int[] d = makeArr();
+    int hap =0;
+    for(int i=0;i<d.length;i++) {
+     hap += d[i];
     	}
     System.out.println(hap);
 	}
-	   // 정수 2개를 더하여 총합을 구하여 리턴하는 메서드를 정의하시오.
+	   private static int[] makeArr() {
+	   int[] arr = new int[3];
+	   arr[0] = 10;
+	   arr[1] = 20;
+	   arr[2] = 30;
+	   
+		return arr;
+	}
+	// 정수 2개를 더하여 총합을 구하여 리턴하는 메서드를 정의하시오.
    public static int sum(int a, int b) {
 	  int v = a +b;
 	return v;
    }
-  public static int[] makeArr() {
-	 int[] arr =new int[3];
-	 arr[0] =10;
-	 arr[1] =20;
-	 arr[2] =30;
-	 return arr;
-  }
+  
 }
