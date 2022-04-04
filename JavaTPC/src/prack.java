6a70eed8d44db54db15903bac1b3af8ac18061e4
dltkdgksqhr <78789ks@naver.com>
2022-04-04 17:46:12 +0900

    22/04/04  TPC11,12 객체가 메모리에 어떻게 만들어지나

--- JavaTPC/src/prack.java
+++ JavaTPC/src/prack.java
@@ -1,10 +1,22 @@
 
+
 public class prack {
 
 	public static void main(String[] args) {
 	TPC09 tpc = new TPC09();
 	int b =tpc.sum(40, 56);
 	System.out.println(b);
+	
+	/*Human hu = new Human();
+	hu.address ="경기도 부천시";
+	hu.height = 175.5;
+	hu.name = "홍길동";
+	hu.phone = "010-2657-9875";
+	
+	System.out.println(hu.address + "\t");
+	System.out.println(hu.height + "\t");
+	System.out.println(hu.name + "\t");
+	System.out.println(hu.phone + "\t");*/
 
 	}
   public int sum(int a, int b) {
