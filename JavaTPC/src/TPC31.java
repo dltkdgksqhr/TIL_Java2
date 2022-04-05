bc6a035a7e5fde8ecb1d5ad4b404c9e4c4e91867
dltkdgksqhr <78789ks@naver.com>
2022-04-05 10:17:47 +0900

    22/04/05  TPC31 private 생성자도 있어요?(static과 관계)

--- JavaTPC/src/TPC31.java
+++ JavaTPC/src/TPC31.java
@@ -9,6 +9,12 @@ public class TPC31 {
 	//inf.java();
 	Inflearn.java(); // static 메서드는 클래스 이름으로 접근
 	
+//	Java API 생성자 private
+//	System sys = new System(); 사용불가
+	System.out.println("출력");
+//	Math m = new Math();
+	int v =Math.max(10, 20);
+	System.out.println(v);
 
 	}
 
