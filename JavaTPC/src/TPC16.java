3cb424b9b60d47bd4cc7ccc3222056d9578be7c2
dltkdgksqhr <78789ks@naver.com>
2022-04-05 15:57:46 +0900

    22/04/05  TPC16  메서드의 오버로딩(Overloading)

---
+++ JavaTPC/src/TPC16.java
@@ -0,0 +1,14 @@
+import kr.tpc.Overload;
+
+public class TPC16 {
+
+	public static void main(String[] args) {
+	// 1+1=?, 23.4+56=?, 56.7+78.9=?
+	Overload ov = new Overload();
+	ov.hap(20, 50); // ov.hap_int_int(20,50)-->컴파일러에서는 이렇게 인식
+	ov.hap(23.4f, 56); // ov.hap_float_int(23.4f,56)
+	ov.hap(56.7f, 78.9f); //ov.hap_float_float()
+
+	}
+
+}
